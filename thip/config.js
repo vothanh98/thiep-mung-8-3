6d6edc7c8b80cac345f3d61c78507e2b26f0d11e
vothanh98 <101181584+vothanh98@users.noreply.github.com>
2022-03-08 17:37:35 +0700

    Add files via upload
    create card

---
+++ thip/config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Bạn';
+const giftUrl = 'https://www.facebook.com/vothanh1998114/';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng bạn';
+const contentCard = 'Chúc Mừng ngày 8.3!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
